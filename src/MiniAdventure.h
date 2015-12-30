7d3d0e690e5270614fd2a9b68483f96a297a643c
Torivon <jonathan@panttaja.com>
2015-12-30 14:21:27 -0800

    I don't recall

--- src/MiniAdventure.h
+++ src/MiniAdventure.h
@@ -25,13 +25,6 @@
 
 #define INCLUDE_SLIDESHOW 0
 
-typedef enum
-{
-	TITLE_MENU = 0,
-	ADVENTURE_MODE,
-	BATTLE_MODE,
-} GameState;
-
 enum
 {
 	DUNGEON_CRAWL_INDEX = 0,
@@ -40,7 +33,7 @@ enum
 };
 
 // Logging
-#define DEBUG_LOGGING 2 // DEBUG_LOGGING 1 turns on DEBUG_LOG. DEBUG_LOGGING 2 turns on DEBUG_VERBOSE_LOG also.
+#define DEBUG_LOGGING 0 // DEBUG_LOGGING 1 turns on DEBUG_LOG. DEBUG_LOGGING 2 turns on DEBUG_VERBOSE_LOG also.
 #define ERROR_LOGGING 1 // ERROR_LOG should be used for actual incorrect operation.
 #define WARNING_LOGGING 1 // WARNING_LOG should be used strange edge cases that are not expected
 #define INFO_LOGGING 1 // INFO_LOG should be used to log game flow.
